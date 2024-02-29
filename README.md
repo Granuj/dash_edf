28a6e3993fde6a917f1889d913cce17f4bfe004c
chris <christophe.say@gmail.com>
2024-02-29 04:30:49 +0100

    Premier callback fonctionnels a rajouter selection par grand secteur 28/02/2024 Rajout radio multiple

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 24-02-2024
 Explo & graphs données (open data) visualisables sous navigateur
+
+manque du tuto :
+Html & CSS  
+Dash design kit (DDK)  
+Dash BootStrap Components  
+Dash Mantine components
+
--- app_Edf.py
+++ app_Edf.py
@@ -4,7 +4,7 @@ import pandas as pd
 import plotly.express as px
 
 df = pd.read_csv('edf_cleaned.csv')
-# df_g = df.groupby(by="libelle_region", as_index=False).sum()
+df_g = df.groupby(by="libelle_grand_secteur", as_index=False).sum()
 # df = df.drop("")
 
 # Initialisation de "app" ?
@@ -15,29 +15,50 @@ app.layout = html.Div([
     html.Div(children='Premier élément du app.layout / html.Div'),
 
     html.Hr(),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=10),
-    #dash_table.DataTable(data=df_g.to_dict('records'), page_size=10),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10),  # "records" arguments ? affiche un df
+    # dash_table.DataTable(data=df_g.to_dict('records'), page_size=10),
 
-    html.Hr(),  # "records" arguments ? affiche un df
+    # Bar plot
+    html.Hr(),
     dcc.Graph(figure=px.histogram(df, x='libelle_region', y='conso', histfunc='sum')),
-    dcc.Graph(figure=px.histogram(df, x="libelle_grand_secteur", y='conso', histfunc='sum')),
+    dcc.Graph(figure=px.histogram(df_g, x="libelle_grand_secteur", y='conso', histfunc='sum')),
 
-    # Radio
+    # Pie chart
+    html.Hr(),
+    dcc.Graph(figure=px.pie(df, values="conso", names="libelle_grand_secteur", hole=0.1)),
+
+    # Radio Bar plot
     # options_radio
     html.Hr(style={'width': '5'}),
-    dcc.RadioItems(options=["conso", "pdl", "indqual"], value="conso", id='controls-and-radio-item'),
-    dcc.Graph(figure={}, id='controls-and-graph')
+    dcc.RadioItems(options=["conso", "pdl", "indqual"], value="conso", id='radio_for_bar'),
+    dcc.Graph(figure={}, id='receiver_bar'),
+
+    # Radio Pie Chart
+    html.Hr(style={'width': '5'}),
+    dcc.RadioItems(options=["conso", "pdl", "indqual"], value="conso", id="radio_for_pie"),
+    dcc.Graph(figure={}, id='receiver_pie'),
 
 ])
 
 
-# Contrôle de Radio
+# Callback de Radio_bar
 @callback(
-    Output(component_id='controls-and-graph', component_property='figure'),
-    Input(component_id='controls-and-radio-item', component_property='value')
+    # Output apparaissant dans component_id de figure
+    Output(component_id='receiver_bar', component_property='figure'),
+    Input(component_id='radio_for_bar', component_property='value')
 )
-def update_graph(col_chosen):
+def update_graph_bar(col_chosen):
     fig = px.histogram(df, x='libelle_region', y=col_chosen, histfunc='sum')
+    return fig
+
+
+# Callback de radio_pie
+@callback(
+    Output(component_id="receiver_pie", component_property='figure'),
+    Input(component_id="radio_for_pie", component_property='value')
+)
+def update_graph_pie(col_chosen):
+    fig = px.pie(df, values=col_chosen, names="libelle_region", hole=0.1)
     return fig
 
 
