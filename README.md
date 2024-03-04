fddc1f1f6f78c8e6e19968b3d57c9694c422095c
chris <christophe.say@gmail.com>
2024-03-04 09:27:14 +0100

    Premier callback fonctionnels a rajouter selection par grand secteur 28/02/2024 Rajout radio multiple 29/02/2024 Premier tuto terminé

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
-# dash_edf
+# Tuto_dash_EDF
+
 
 24-02-2024
-Explo & graphs données (open data) visualisables sous navigateur
+Explo & graphs données (open data) visualisables sous navigateur  
+
+Données depuis :  
+https://www.data.gouv.fr/fr/pages/donnees-energie/
+
 
 manque du tuto :  
 Dash design kit (DDK)  'problème installation'  
 Dash BootStrap Components  
-Dash Mantine components
+
+- PCA
+- régression linéaire de qualitépoint selon conso et nombre de point
+- séléction de méthode de régression
+- Carte de france montrant les nombres, consomation et qualité de point.
+
+    # classname utilité ?
+    # argument records dans .to_dict()?
--- app_Edf.py
+++ app_Edf.py
@@ -1,82 +1,90 @@
-# Importation bibliothèques et données
+# Importation bibliothèques
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
 
+# Importation data
 df = pd.read_csv('edf_cleaned.csv')
-df_g = df.groupby(by="libelle_grand_secteur", as_index=False).sum()
-# df = df.drop("")
+dfg = df.groupby(by="libelle_grand_secteur", as_index=False).sum()
+for_pca = df[["conso", "pdl", "indqual"]]
 
-# Initialisation de "app" ?
+# Initialisation de "app"
 app = Dash(__name__)
 
-# Layout / corps de "app"
-app.layout = html.Div([
-    html.Div(className='row', children='Infos sur les distributeurs et leurs points de livraisons',
-             style={'textAlign': 'center', 'color': 'darkblue', 'fontSize': 30, 'backgroundColor': 'lightblue'}),
-    # classname utilité ?
+# Corps principal / Layout html de "app"
+app.layout = html.Div([  # Élément Div contenant tous les autres éléments de Layout
 
-    html.Hr(style={'width': '50', "color": "darkblue"}),
-    html.Div(children="Table de base"),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=10),  # "records" arguments ? affiche un df
-    # dash_table.DataTable(data=df_g.to_dict('records'), page_size=10),
+    # Titrage
+    html.Div(className="row", children="Information sur la distribution de l'électricité en France",
+             # style={"textAlign": "center", "color": "white", "fontSize": 35, "backgroundColor": "lightblue"}),
+             style={"textAlign": "center", "color": "white", "fontSize": 35, "backgroundColor": "rgb(0,128,255)"}),
 
-    # Bar plot
-    html.Hr(),
-    html.Div(children="BarPlot"),
-    dcc.Graph(figure=px.histogram(df, x='libelle_region', y='conso', histfunc='sum')),
-    dcc.Graph(figure=px.histogram(df_g, x="libelle_grand_secteur", y='conso', histfunc='sum')),
+    # Bar de séparation
+    html.Hr(style={"width": 500, "border-color": "red", }),
+    # Titrage
+    html.Br(),  # Saut à la ligne (linebreak)
+    html.Div(children="DataFrame nettoyé", style={"textAlign": "center"}),
+    html.Div(children="(18128 individus, 15 variables)", style={"textAlign": "center"}),
+    html.Br(),
+    # Affichage du DataFrame
+    # dash_table.DataTable(data=df.to_dict("records"), page_size=10), # sans css ?
+    html.Div(className="six columns", children=[
+        dash_table.DataTable(data=df.to_dict("records"), page_size=10, style_table={"overflowX": "auto"})]),
 
-    # Pie chart
-    html.Hr(),
-    html.Div(children="PieChart"),
-    dcc.Graph(figure=px.pie(df, values="conso", names="libelle_grand_secteur", hole=0.1)),
+    # Bar de séparation
+    html.Hr(style={"width": 500, "border-color": "red", }),
+    # Titrage
+    html.Br(),  # Saut à la ligne (linebreak)
+    html.Div(children="Scatterplot", style={"textAlign": "center"}),
+    html.Br(),
+    html.Div(children="X = conso, Y = indqual Z = pdl", style={"textAlign": "center"}),
+    html.Div(dcc.Graph(figure=px.scatter(x=df["conso"], y=df["indqual"], labels={"x": "conso", "y": "indqual"}))),
+    html.Div(dcc.Graph(figure=px.scatter_3d(
+        x=df["conso"], y=df["indqual"], z=df["pdl"], labels={"x": "conso", "y": "indqual", "z": "pdl"}))),
 
-    # Interactif
-    html.Hr(style={'width': '5'}),
-    html.Div(children="Radio & @callback"),
-    # Radio Bar plot
-    # options_radio
-    dcc.RadioItems(options=["conso", "pdl", "indqual"], value="conso",inline=True, id='radio_for_bar'),
-    dcc.Graph(figure={}, id='receiver_bar'),
+    # Bar de séparation
+    # html.Div(html.Hr(style={"width": 500, "border-color": "lightblue", })), # peut être mis dans un .div
+    html.Hr(style={"width": 500, "border-color": "red", }),
+    # Titrage
+    html.Br(),
+    html.Div(children="Répartition par région", style={"textAlign": "center"}),
+    html.Br(),
+    # Commande dropdown_bar
+    dcc.Dropdown(options=["conso", "pdl", "indqual"], value="conso", id="input_Dropdown_bar"),
+    dcc.Graph(figure={}, id="receiver_Dropdown_bar"),
+    # Commande dropdown_bar
+    # dcc.RadioItems(options=["conso", "pdl", "indqual"], value="conso", inline=True, id="input_RadioItems_pie"),
+    dcc.Graph(figure={}, id="receiver_Dropdown_pie"),
 
-    # Radio Pie Chart
-    html.Hr(style={'width': '50', "color": "darkblue"}),
-    dcc.RadioItems(options=["conso", "pdl", "indqual"], value="conso",id="radio_for_pie"),
-    dcc.Graph(figure={}, id='receiver_pie'),
+    # Bar de séparation
+    html.Hr(style={"width": 500, "border-color": "red", }),
+    # Titrage
+    html.Br(),  # Saut à la ligne (linebreak)
+    html.Div(children="PCA", style={"textAlign": "center"}),
+    html.Br(),
 
-    # Essai css multiple
-    html.Hr(style={'width': '50', "color": "darkblue"}),
-    html.Div(children="Essai css multiple"),
-
-    html.Div(className="row", children=[
-        html.Div(className="six columns", children=[dash_table.DataTable(data=df.to_dict('records'), page_size=10, style_table={'overflowX': 'auto'}),]),
-        html.Div(className="six columns", children=[dcc.Graph(figure=px.pie(df, values="conso", names="libelle_grand_secteur", hole=0.1))])
-    ])
 ])
 
 
-# Callback de Radio_bar
+# callback de drop_Down
 @callback(
-    # Output apparaissant dans component_id de figure
-    Output(component_id='receiver_bar', component_property='figure'),
-    Input(component_id='radio_for_bar', component_property='value')
+    Output(component_id="receiver_Dropdown_bar", component_property="figure"),
+    Input(component_id="input_Dropdown_bar", component_property="value")
 )
-def update_graph_bar(col_chosen):
-    fig = px.histogram(df, x='libelle_region', y=col_chosen, histfunc='sum')
+def update_figure(col_chosen):
+    fig = px.histogram(df, x="libelle_region", y=col_chosen, histfunc="sum")
     return fig
 
 
-# Callback de radio_pie
 @callback(
-    Output(component_id="receiver_pie", component_property='figure'),
-    Input(component_id="radio_for_pie", component_property='value')
+    Output(component_id="receiver_Dropdown_pie", component_property="figure"),
+    Input(component_id="input_Dropdown_bar", component_property="value")
 )
-def update_graph_pie(col_chosen):
+def update_figure(col_chosen):
     fig = px.pie(df, values=col_chosen, names="libelle_region", hole=0.1)
     return fig
 
 
-# permet de lancer / run "app"
-if __name__ == '__main__':
+# Lancement de "app"
+if __name__ == "__main__":
     app.run(debug=True)
